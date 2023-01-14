526b42b9b296b90327b1795b7e5e614d634e4fad
Dinesh Chukkala <dineshchukkala169@gmail.com>
2023-01-14 15:53:20 +0530

    Made first migration

---
+++ prisma/migrations/20230114101706_initialisation/migration.sql
@@ -0,0 +1,113 @@
+-- CreateTable
+CREATE TABLE `Customer` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `address` VARCHAR(191) NOT NULL,
+    `latitude` VARCHAR(191) NOT NULL,
+    `longitude` VARCHAR(191) NOT NULL,
+    `phoneno` VARCHAR(20) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Product` (
+    `SKU` VARCHAR(50) NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `desc` VARCHAR(191) NULL,
+    `density` FLOAT NOT NULL,
+
+    PRIMARY KEY (`SKU`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Rider` (
+    `id` VARCHAR(50) NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `phoneno` VARCHAR(20) NULL,
+    `onduty` BOOLEAN NOT NULL,
+    `email` VARCHAR(50) NOT NULL,
+    `password` VARCHAR(50) NOT NULL,
+    `driving_license` VARCHAR(191) NULL,
+    `blood_group` ENUM('A_POSITIVE', 'A_NEGATIVE', 'B_POSITIVE', 'B_NEGATIVE', 'AB_POSITIVE', 'AB_NEGATIVE', 'O_POSITIVE', 'O_NEGATIVE', 'OTHER') NULL,
+
+    UNIQUE INDEX `Rider_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Inventory` (
+    `id` VARCHAR(50) NOT NULL,
+    `customer_id` INTEGER NOT NULL,
+    `product_id` VARCHAR(50) NOT NULL,
+    `AWB` VARCHAR(50) NOT NULL,
+    `shipped` BOOLEAN NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Delivery` (
+    `id` VARCHAR(20) NOT NULL,
+    `rider_id` VARCHAR(50) NOT NULL,
+    `customer_id` INTEGER NOT NULL,
+    `item_id` VARCHAR(50) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Archive` (
+    `id` VARCHAR(50) NOT NULL,
+    `customer_id` INTEGER NOT NULL,
+    `product_id` VARCHAR(50) NOT NULL,
+    `AWB` VARCHAR(50) NOT NULL,
+    `delivery_id` VARCHAR(20) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Pickup` (
+    `id` VARCHAR(20) NOT NULL,
+    `rider_id` VARCHAR(50) NOT NULL,
+    `customer_id` INTEGER NOT NULL,
+    `item_id` VARCHAR(50) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Inventory` ADD CONSTRAINT `Inventory_customer_id_fkey` FOREIGN KEY (`customer_id`) REFERENCES `Customer`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Inventory` ADD CONSTRAINT `Inventory_product_id_fkey` FOREIGN KEY (`product_id`) REFERENCES `Product`(`SKU`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Delivery` ADD CONSTRAINT `Delivery_rider_id_fkey` FOREIGN KEY (`rider_id`) REFERENCES `Rider`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Delivery` ADD CONSTRAINT `Delivery_customer_id_fkey` FOREIGN KEY (`customer_id`) REFERENCES `Customer`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Delivery` ADD CONSTRAINT `Delivery_item_id_fkey` FOREIGN KEY (`item_id`) REFERENCES `Inventory`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Archive` ADD CONSTRAINT `Archive_customer_id_fkey` FOREIGN KEY (`customer_id`) REFERENCES `Customer`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Archive` ADD CONSTRAINT `Archive_product_id_fkey` FOREIGN KEY (`product_id`) REFERENCES `Product`(`SKU`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Archive` ADD CONSTRAINT `Archive_delivery_id_fkey` FOREIGN KEY (`delivery_id`) REFERENCES `Delivery`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Pickup` ADD CONSTRAINT `Pickup_rider_id_fkey` FOREIGN KEY (`rider_id`) REFERENCES `Rider`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Pickup` ADD CONSTRAINT `Pickup_customer_id_fkey` FOREIGN KEY (`customer_id`) REFERENCES `Customer`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Pickup` ADD CONSTRAINT `Pickup_item_id_fkey` FOREIGN KEY (`item_id`) REFERENCES `Inventory`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
